ac30178450b21786f6c439d08d3c4e9103c4e3fb
Alba <110181091+AMorQ@users.noreply.github.com>
2023-02-02 13:13:34 +0100

    Update toy_example.py
    Update toy_example for correct implementation

--- toy_example.py
+++ toy_example.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-x_train = np.linspace(0, 1, 15)
-third = int(len(x_train)/3)
-y_train = np.array(third*[0] + third*[1] + third*[2])
-bag_indexes  = np.array((third-2)*[0] + 1*[1] + 1*[2] + third*[1] +
-third*[2])
-bag_label = bag_indexes
+def toy_example(N):
+   x_train = np.linspace(0, 1, N)#total amount of items must be multiple of 3, but 3
+   third = int(len(x_train)/3)
+   y_train = np.array(third*[0] + third*[1] + third*[2])
+   bag_indexes = np.array((third-2)*[0] + 1*[1] + 1*[2] + third*[1] +
+   third*[2])
+   bag_label = bag_indexes
 
-for b in range(3):
-   print(x_train[(bag_indexes==b)])
-   print(y_train[bag_indexes==b])
-   print(bag_indexes[bag_indexes==b])
+   for b in range(3):
+      print(x_train[(bag_indexes==b)])
+      print(y_train[bag_indexes==b])
+      print(bag_indexes[bag_indexes==b])
+
+
+   x_train = x_train.reshape(-1, 1)
+   y_train = y_train.reshape(-1, 1)
+
+   return x_train, y_train, bag_label
